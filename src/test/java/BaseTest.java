900d0d7e3c99626e559fc22c2cb9e50ab0db7128
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-15 14:03:20 -0400

    LAHomework15 submit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.BeforeMethod;
@@ -10,9 +11,14 @@ import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 
+    static WebDriver driver = null;
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
     }
 
 }
---
+++ src/test/java/LAHomework15.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class LAHomework15 extends BaseTest {
+
+    @Test
+    public void Navigation() {
+        String url = "https://testpro.io/";
+        driver.get(url);
+
+    }
+}
