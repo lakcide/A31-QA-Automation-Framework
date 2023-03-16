44dad836748c11d0714643f673af51dfbb89684a
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-16 14:39:43 -0400

    LAHomework15 update

--- src/test/java/LAHomework15.java
+++ src/test/java/LAHomework15.java
@@ -4,6 +4,7 @@ public class LAHomework15 extends BaseTest {
 
     @Test
     public void Navigation() {
+//      String url = "https://bbb.testpro.io/";
         String url = "https://testpro.io/";
         driver.get(url);
 
