2da73566f9e204a6dd1679bc43a852d86b23f01c
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-16 15:48:27 -0400

    LAHomework15 update - submit

--- src/test/java/LAHomework15.java
+++ src/test/java/LAHomework15.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LAHomework15 extends BaseTest {
@@ -6,6 +7,7 @@ public class LAHomework15 extends BaseTest {
     public void Navigation() {
 //        String url = "https://bbb.testpro.io/";
         String url = "https://testpro.io/";
+        Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.get(url);
 
     }
