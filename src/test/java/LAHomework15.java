a4206210cd5c40d9c157831afaa41910b2b84add
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-16 23:48:08 -0400

    15 update

--- src/test/java/LAHomework15.java
+++ src/test/java/LAHomework15.java
@@ -4,8 +4,8 @@ import org.testng.annotations.Test;
 public class LAHomework15 extends BaseTest {
 
     @Test
-    public void Navigation() throws InterruptedException {
-//      String url = "https://bbb.testpro.io/";
+    public void Navigation() throws Exception {
+//        String url = "https://bbb.testpro.io/";
         String url = "https://testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
