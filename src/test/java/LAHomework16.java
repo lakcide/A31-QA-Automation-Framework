f483a07c43b50d4516d56623d7ecbe7b17340437
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-16 23:32:53 -0400

    LAHomework16 update

--- src/test/java/LAHomework16.java
+++ src/test/java/LAHomework16.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-<<<<<<< Updated upstream
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,36 +8,26 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 import static java.lang.String.*;
-=======
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
->>>>>>> Stashed changes
 
 public class LAHomework16 extends BaseTest{
 
-        @Test
-        public void Navigation () throws InterruptedException {
+    @Test
+    public void Navigation () throws InterruptedException {
 
-            String url = "https://bbb.testpro.io/";
-            driver.get(url);
-            Thread.sleep(2000);
-            WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
-            registrationLink.click();
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        Thread.sleep(2000);
+        WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
+        registrationLink.click();
 
-            String registrationUrl = "https://bbb.testpro.io/registration.php";
-            Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
-            Thread.sleep(2000);
-            driver.quit();
+        String registrationUrl = "https://bbb.testpro.io/registration.php";
+        Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
+        Thread.sleep(2000);
+        driver.quit();
 
-<<<<<<< Updated upstream
 //            https://bbb.testpro.io/registration.php
-
-=======
->>>>>>> Stashed changes
-        }
 
     }
 
+}
 
