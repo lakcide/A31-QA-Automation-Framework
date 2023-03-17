4c30189b510aa01d8961fe8a505fef1a6a81d1a8
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-16 23:14:13 -0400

    LAHomework17 update

--- src/test/java/LAHomework17.java
+++ src/test/java/LAHomework17.java
@@ -4,14 +4,12 @@ import org.testng.annotations.Test;
 
 public class LAHomework17 extends BaseTest {
     @Test
-    public void Navigation () throws InterruptedException {
+    public void Navigation() throws InterruptedException {
         String url = "https://bbb.testpro.io/";
         driver.get(url);
         Thread.sleep(2000);
         WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
         registrationLink.click();
-
-        String registrationUrl = "https://bbb.testpro.io/registration.php";
     }
-
 }
+
