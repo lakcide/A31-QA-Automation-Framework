0f2f1c2eab36fc0116b02d24801c3436174f4602
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-22 14:47:38 -0400

    LA Homework17 submit
    Updating of homework and BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,24 +1,91 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+import java.util.UUID;
+
 
 public class BaseTest {
+    public static WebDriver driver = null;
+    public static String url = "https://bbb.testpro.io/";
+    public static Actions act = null;
 
-    static WebDriver driver = null;
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
     }
 
+    @BeforeMethod
+    public static void launchBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        LoginTests.driver = new ChromeDriver(options);
+        LoginTests.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        LoginTests.driver.manage().window().maximize(); //maximizes the browser window
+        act = new Actions (LoginTests.driver);
+    }
+
+    @AfterMethod
+    public static void closeBrowser(){
+        LoginTests.driver.quit();
+    }
+
+    protected static void navigateToPage() {
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+    }
+
+    public static void clickSubmit() throws InterruptedException {
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+        Thread.sleep(2000); //pauses test script execution for 2seconds
+    }
+
+    public static void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public static void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public static void clickSaveButton() {
+        WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
+        saveButton.click();
+    }
+
+    public static void provideProfileName(String randomName) {
+        WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
+        profileName.clear();
+        profileName.sendKeys(randomName);
+    }
+
+    public static void provideCurrentPassword(String password) {
+        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+        currentPassword.clear();
+        currentPassword.sendKeys(password);
+    }
+
+    public static String generateRandomName() {
+        return UUID.randomUUID().toString().replace("-", "");//
+    }
+
+    public static void clickAvatarIcon() {
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+        avatarIcon.click();
+
+    }
 }
--- src/test/java/LaHomework17.java
+++ src/test/java/LaHomework17.java
@@ -1,33 +1,79 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LaHomework17 extends BaseTest {
+//    @Test
+//    public void Navigation() throws InterruptedException {
+//        String url = "https://bbb.testpro.io/";
+//        driver.get(url);
+//        driver.findElement(By.cssSelector("[type='email']"));
+//        provideEmail("lakcide@gmail.com");
+//        driver.findElement(By.cssSelector("[type='password']"));
+//        providePassword("te$t$tudent");
+//        clickSubmit();
+//
+//        String homePageUrl = "https://bbb.testpro.io/#!/home";
+//        Thread.sleep(2000);
+//        Assert.assertEquals(driver.getCurrentUrl(), (homePageUrl));
+//    }
+//    we can remove the test above
+
     @Test
-    public void Navigation() throws InterruptedException {
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-        driver.findElement(By.cssSelector("[type='email']"));
-        provideEmail("lakcide@gmail.com");
-        driver.findElement(By.cssSelector("[type='password']"));
+    public void addSongToPlaylist() throws InterruptedException {//so this will be the homework17 test case
+        //hw17 steps
+
+        String newSongAddedText = "Added 1 song into";
+
+//        navigate to koel
+        navigateToPage();
+//        login to koel
+        provideEmail("demo@class.com");
         providePassword("te$t$tudent");
         clickSubmit();
-
-        String homePageUrl = "https://bbb.testpro.io/#!/home";
+        //choose song to add
+        selectSong();
         Thread.sleep(2000);
-        Assert.assertEquals(driver.getCurrentUrl(), (homePageUrl));
+        //click Add to button
+        clickAddTo();
+        Thread.sleep(2000);
+        //choose playlist to add the song to
+        songToPlaylist("Favorite Playlist");
+
+        Assert.assertTrue(getAddedSongNotificationMsg().contains(newSongAddedText));
+
     }
-    @Test
-    public void selectSong () {
+
+
+//    @Test//remove @Test for methods,
+    public void selectSong () throws InterruptedException {
         driver.findElement(By.cssSelector("[href='#!/songs']")).click();
-        driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/div/div/div[1]/table/tr[2]/td[2]")).click();
+        Thread.sleep(3000);
+       WebElement secondSong = driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/div/div/div[1]/table/tr[2]/td[2]"));
+       secondSong.click();
     }
-    @Test
-    public void songToPlaylist () {
-        Actions act = new Actions (driver);
-        act.moveToElement(driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/div/div/div[1]/table/tr[2]/td[2]"))).perform();
-        act.release(driver.findElement(By.cssSelector("[href='#!/playlist/22346']"))).release();
+//    @Test//remove @Test for methods
+    public void songToPlaylist (String playlistName) throws InterruptedException {
+
+//        act.moveToElement(driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/div/div/div[1]/table/tr[2]/td[2]"))).perform();
+//        act.release(driver.findElement(By.cssSelector("[href='#!/playlist/22346']"))).release();
+//        drag drop not working for koel
+
+        WebElement playlistElement = driver.findElement(By.xpath("//section[@id='songsWrapper']//li[contains(text(),'"+playlistName+"')]"));
+        playlistElement.click();
+        Thread.sleep(2000);
+        }
+
+        public void clickAddTo(){
+            driver.findElement(By.cssSelector(".btn-add-to")).click();
+//            Assert.assertEquals(driver.getPageSource ("Favorite Playlist") );
+        }
+
+        public String getAddedSongNotificationMsg(){
+        //so we created a method which will return a string, (the pop up message)
+            WebElement popUpMsg = driver.findElement(By.cssSelector("div.success.show"));
+            return popUpMsg.getText();
         }
 
     }
