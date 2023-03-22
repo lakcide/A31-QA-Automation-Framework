45a5336892ba8b27cf0c6e695de3a5b1a590a74d
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-22 15:22:03 -0400

    Rename HW15

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,17 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 
 
 public class BaseTest {
 
     static WebDriver driver = null;
 
-    @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
