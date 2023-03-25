b7d89a8d961a89c7b72eac588162395cbf5fd734
lakcide <121534553+lakcide@users.noreply.github.com>
2023-03-25 17:46:22 -0400

    LA-Homework18 submit
    LA-Homework18 submit

--- src/test/java/LaHomework18.java
+++ src/test/java/LaHomework18.java
@@ -5,19 +5,21 @@ import org.testng.annotations.Test;
 public class LaHomework18 extends BaseTest {
 
     @Test
-    public void playASong () throws InterruptedException {
+    public void playSong () throws InterruptedException {
         navigateToPage();
         login();
-        playSong();
 
-    }
-    public void playSong () {
-        WebElement selectSong = driver.findElement(By.cssSelector("[href='#!/songs']"));
-        String playlistName = null;
-        WebElement playlistElement = driver.findElement(By.xpath("//section[@id='songsWrapper']//li[contains(text(),'"+playlistName+"')]"));
-        playlistElement.click();
+        driver.findElement(By.cssSelector("[href='#!/songs']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/div/div/div[1]/table/tr[2]/td[2]")).click();
+        WebElement selectPlayButton = driver.findElement(By.xpath("//span[@data-testid=\"play-btn\"]"));
+        selectPlayButton.click();
+        Thread.sleep(2000);
+
+//        WebElement playNxtBtn = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+//        playNxtBtn.click();
+        WebElement songPlays = driver.findElement(By.xpath("[alt=\"Sound bars\"]"));
 //        [class="playback"]
-
     }
 
 }
